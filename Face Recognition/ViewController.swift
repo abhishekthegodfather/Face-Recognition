c998e43ce7f4f0f31c84a1182290fbc6984f6208
Abhishek Biswas <abhishekbiswas772@gmail.com>
2023-08-01 11:29:17 +0530

    Initial Commit

---
+++ Face Recognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Face Recognition
+//
+//  Created by Abhishek Biswas on 01/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Face RecognitionUITests/Face_RecognitionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Face_RecognitionUITestsLaunchTests.swift
+//  Face RecognitionUITests
+//
+//  Created by Abhishek Biswas on 01/08/23.
+//
+
+import XCTest
+
+final class Face_RecognitionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
